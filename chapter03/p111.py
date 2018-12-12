19baf617e171dc37d55a8ed38687ce325c9c041f
yangju0411 <yangju0411@gmail.com>
2018-12-12 13:30:14 +0900

    이제까지 한 것들

---
+++ chapter03/p111.py
@@ -0,0 +1,3 @@
+data = open('C:\Git_project\HFPython\chapter03\sketch.txt')
+for each_line in data:
+    print(each_line, end = '')
---
+++ chapter04/p143.py
@@ -0,0 +1,26 @@
+###
+man = []
+other = []
+try:
+    data = open('C:\Git_project\HFPython\chapter04\sketch.txt')
+    for each_line in data:
+        try:
+            (role, line_spoken) = each_line.split(':', 10)
+            ###
+            line_spoken = line_spoken.strip()
+            if role == 'Man':
+                man.append(line_spoken)
+            else:
+                other.append(line_spoken)
+        except ValueError:
+            pass
+    data.close()
+except IOError:
+    print('The datafile is missing!!')
+try:
+    with open("C:\Git_project\HFPython\chapter04\man_data.txt", 'w') as man_data:
+        print(man, file = man_data)
+    with open("C:\Git_project\HFPython\chapter04\other_data.txt", 'w') as other_data:
+        print(other, file = other_data)  
+except IOError as err:
+    print('File error' + str(err))
