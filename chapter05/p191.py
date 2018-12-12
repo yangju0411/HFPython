62ced8e1a047dd35b540a7777c2e1a7586dc0af0
yangju0411 <yangju0411@gmail.com>
2018-12-12 22:30:26 +0900

    지능형 리스트를 이용해서 p187의 코드를 한 줄로 줄임

---
+++ chapter05/p191.py
@@ -0,0 +1,15 @@
+from sanitize import sanitize
+with open('C:\Git_project\HFPython\chapter05\james.txt') as james_data:
+    data = james_data.readline()
+James = data.strip().split(',')
+with open('C:\Git_project\HFPython\chapter05\julie.txt') as julie_data:
+    data = julie_data.readline()
+Julie = data.strip().split(',')
+with open('C:\Git_project\HFPython\chapter05\mikey.txt') as mikey_data:
+    data = mikey_data.readline()
+Mikey = data.strip().split(',')
+with open('C:\Git_project\HFPython\chapter05\sarah.txt') as sarah_data:
+    data = sarah_data.readline()
+Sarah = data.strip().split(',')
+New_James = [sanitize(each_t) for each_t in James]
+print(New_James)
