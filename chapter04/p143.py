e4a42f1ba73be082458c87eb0b7c19e0ba2f30e5
yangju0411 <yangju0411@gmail.com>
2018-12-12 17:35:02 +0900

    책과 내용 맞춤

--- chapter04/p143.py
+++ chapter04/p143.py
@@ -5,7 +5,7 @@ try:
     data = open('C:\Git_project\HFPython\chapter04\sketch.txt')
     for each_line in data:
         try:
-            (role, line_spoken) = each_line.split(':', 10)
+            (role, line_spoken) = each_line.split(':')
             ###
             line_spoken = line_spoken.strip()
             if role == 'Man':
@@ -18,9 +18,8 @@ try:
 except IOError:
     print('The datafile is missing!')
 try:
-    with open("C:\Git_project\HFPython\chapter04\man_data.txt", 'w') as man_data:
-        print(man, file = man_data)
-    with open("C:\Git_project\HFPython\chapter04\other_data.txt", 'w') as other_data:
+    with open("C:\Git_project\HFPython\chapter04\man_data.txt", 'w') as man_data, open("C:\Git_project\HFPython\chapter04\other_data.txt", 'w') as other_data:
+        print(man, file = man_data) 
         print(other, file = other_data)  
 except IOError as err:
     print('File error' + str(err))
--- chapter04/p165.py
+++ chapter04/p165.py
@@ -1,3 +1,26 @@
 import p163
-with open('C:\Git_project\HFPython\chapter04\man_data.txt') as mdf:
-	p163.print_lol(mdf)
+###
+man = []
+other = []
+try:
+    data = open('C:\Git_project\HFPython\chapter04\sketch.txt')
+    for each_line in data:
+        try:
+            (role, line_spoken) = each_line.split(':')
+            ###
+            line_spoken = line_spoken.strip()
+            if role == 'Man':
+                man.append(line_spoken)
+            else:
+                other.append(line_spoken)
+        except ValueError:
+            pass
+    data.close()
+except IOError:
+    print('The datafile is missing!')
+try:
+    with open("C:\Git_project\HFPython\chapter04\man_data.txt", 'w') as man_data, open("C:\Git_project\HFPython\chapter04\other_data.txt", 'w') as other_data:
+        p163.print_lol(man, fh = man_data)
+        p163.print_lol(other, fh = other_data)
+except IOError as err:
+    print('File error' + str(err))
