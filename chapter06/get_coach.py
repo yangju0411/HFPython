7291a22d964a2eaa23f4a9469042bbcf266988bd
yangju0411 <yangju0411@gmail.com>
2018-12-14 15:54:08 +0900

    6장 필요 파일들

---
+++ chapter06/get_coach.py
@@ -0,0 +1,8 @@
+def get_coach_data(filename):
+    try:
+        with open(filename) as f:
+            data = f.readline()
+            return(data.strip().split(','))
+    except IOError as ioerr:
+        print('File error:' + str(ioerr))
+        return(None)
