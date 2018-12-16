88f9e3c3b2a4f1566eeb075131cf72747943eaf3
yangju0411 <yangju0411@gmail.com>
2018-12-16 14:35:13 +0900

    내장 클래스인 list에서 상속받은 클래스 만들기

---
+++ chapter06/p242.py
@@ -0,0 +1,13 @@
+class NamedList(list):
+    def __init__(self, a_name):
+        list.__init__([])
+        self.name = a_name
+johnny = NamedList("John Paul Jones")
+print(type(johnny))
+print(dir(johnny))
+johnny.append("Base Player")
+johnny.extend(['Composer', 'Arranger', 'Musician'])
+print(johnny)
+print(johnny.name)
+for attr in johnny:
+    print(johnny.name + ' is a ' + attr + '.')
